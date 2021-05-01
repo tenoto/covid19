86dfe7515b4d4a60b0a0c5fad1f2e962ed9095c7
Teruaki Enoto <teruaki.enoto@riken.jp>
2021-05-01 09:38:00 +0900

    plot

--- script/plot_curve_vaccine.py
+++ script/plot_curve_vaccine.py
@@ -15,6 +15,7 @@ figsize_y = 9
 date_start = '2021-02-15'
 #date_end = '2021-04-29'
 date_end = date.today()
+date_of_end_of_emergency = '2021-05-11'
 normalization = 10000
 fit_width_days = 14
 prediction_month = 8 
@@ -142,10 +143,10 @@ fig.savefig("fig/covid19_vaccine_daily_1st.jpg",dpi=300)
 fig = plt.figure(figsize=(figsize_x,figsize_y),tight_layout=True)
 ax1 = fig.add_subplot(111)
 ax1.fill_between(pd.to_datetime(date_array),num2nd_array/normalization,
-	step="mid",color='#F08080',alpha=0.5)
+	step="mid",color='#85C1E9',alpha=0.5)
 ax1.step(pd.to_datetime(date_array),num2nd_array/normalization,
 	'-',where='mid',markersize=0,label='合計(医療従事者+高齢者)',
-	color='r')
+	color='b')
 ax1.plot(df_korei['date'],df_korei['#2nd']/normalization,'o-',
 	markersize=8,label='高齢者')
 #plt.yscale('log')
@@ -187,35 +188,43 @@ def number2ratio(x):
 def ratio2number(x):
     return x / normalization / 100 * total_population
 
+zorder = 0 
+
 fig = plt.figure(figsize=(figsize_x,figsize_y),tight_layout=True)
 ax1 = fig.add_subplot(111)
 ax1.fill_between(pd.to_datetime(date_array),np.add.accumulate(num1st_array)/normalization,
-	step="mid",color='#F08080',alpha=0.5)
+	step="mid",color='#F08080',alpha=0.5,label='１回目の接種人数',zorder=zorder);zorder+=1
 ax1.step(pd.to_datetime(date_array),np.add.accumulate(num1st_array)/normalization,
-	'-',where='mid',markersize=0,label='合計　(医療従事者+高齢者)',
-	color='r')
+	'-',where='mid',markersize=0,color='r',zorder=zorder);zorder+=1
 #ax1.plot(df_korei['date'],np.add.accumulate(df_korei['#1st'])/normalization,'o-',
 #	markersize=8,label='高齢者')
 #plt.yscale('log')
+ax1.fill_between(pd.to_datetime(date_array),np.add.accumulate(num2nd_array)/normalization,
+	step="mid",color='#85C1E9',alpha=0.5,label='２回目の接種人数',zorder=zorder);zorder+=1
+ax1.step(pd.to_datetime(date_array),np.add.accumulate(num2nd_array)/normalization,
+	'-',where='mid',markersize=0,color='b',zorder=zorder);zorder+=1
 ax1.plot(prediction_date,
-	prediction_shots,linestyle="--",color='k',
-	label='直近 %d 日間の１次関数フィット (+%.1f 万人/日)' % (fit_width_days,pol1_fit_param[0]))
+	prediction_shots,linestyle="--",color='k',zorder=zorder,
+	label='直近 %d 日間の１次フィット (+%.1f 万人/日)' % (fit_width_days,pol1_fit_param[0]));
+zorder+=1
 ax1.set_xlim(date_start,date_end)
+ax1.set_ylim(0,1.2*max(np.add.accumulate(num1st_array)/normalization))
 ax1.set_xlabel('日付 (2021年)')
-ax1.set_ylabel('積算の接種人数 (万人)',labelpad=20)
+ax1.set_ylabel('積算のワクチン接種人数 (万人)',labelpad=20)
 myFmt = mdates.DateFormatter('%m/%d')
 ax1.xaxis.set_major_formatter(myFmt)
 ax1.legend(loc='upper left',borderaxespad=1,fontsize=20,ncol=1,
-	title='１回目ワクチン接種者数\n (1日毎の積算値, %s時点)' % date.today())
+	title='首相官邸・厚生労働省のサイトから作成\n (1日毎の積算値, %s時点)' % date.today())
 fig.patch.set_alpha(0.0)
 ax1.patch.set_alpha(0.0) 
-plt.gca().set_ylim(bottom=0)
+#plt.gca().set_ylim(bottom=0)
 second_ay = ax1.secondary_yaxis('right',functions=(number2ratio, ratio2number))
 second_ay.set_ylabel('人口に占める割合 (%)')
-fig.savefig("fig/covid19_vaccine_accum_1st.pdf")
-fig.savefig("fig/covid19_vaccine_accum_1st.jpg",dpi=300)
+fig.savefig("fig/covid19_vaccine_accum.pdf")
+fig.savefig("fig/covid19_vaccine_accum.jpg",dpi=300)
 
 
+"""
 ################
 # Accumulation 1st, Olympic
 ################
@@ -225,7 +234,6 @@ def number2ratio_oku(x):
 
 def ratio2number_oku(x):
     return x / normalization**2 / 100 * total_population
-
 
 fig = plt.figure(figsize=(figsize_x,figsize_y),tight_layout=True)
 ax1 = fig.add_subplot(111)
@@ -268,7 +276,7 @@ second_ay = ax1.secondary_yaxis('right',functions=(number2ratio_oku, ratio2numbe
 second_ay.set_ylabel('人口に占める割合 (%)')
 fig.savefig("fig/covid19_vaccine_accum_1st_prediction.pdf")
 fig.savefig("fig/covid19_vaccine_accum_1st_prediction.jpg",dpi=300)
-
+"""
 
 
 ################
@@ -278,58 +286,77 @@ fig.savefig("fig/covid19_vaccine_accum_1st_prediction.jpg",dpi=300)
 fig = plt.figure(figsize=(figsize_x,figsize_y),tight_layout=True)
 ax1 = fig.add_subplot(111)
 
-#for i in range(5):
-#	j = i+1
-#	ax1.axvspan(
-#		pd.to_datetime('2021-%02d-01' % (2*j)), 
-#		pd.to_datetime('2021-%02d-01' % (2*j+1)),
-#		facecolor='#E5E7E9')
-
 zorder = 0
 for i in range(1,12):
 	plt.axvline(pd.to_datetime('2021-%02d-01' % i), 
 		color='#E5E7E9',linestyle='-.',zorder=zorder)
 	zorder += 1
 
+ax1.axvspan(pd.to_datetime('2021-04-25'),pd.to_datetime(date_of_end_of_emergency),
+	color="#FCF3CF",zorder=zorder,label='緊急事態宣言 (%s まで)' % date_of_end_of_emergency);zorder+=1
+ax1.axvspan(pd.to_datetime('2021-01-08'),pd.to_datetime('2021-03-21'),
+	color="#FCF3CF",zorder=zorder);zorder+=1
+#plt.text(pd.to_datetime('2021-05-01'),50,
+#	'緊急事態宣言',color='k',ha='center', 
+#	va='center',rotation='vertical',zorder=zorder);zorder+=1
+# ====
 ax1.axvspan(pd.to_datetime('2021-07-23'),pd.to_datetime('2021-08-08'),
 	color="#AED6F1",zorder=zorder);zorder+=1
 plt.text(pd.to_datetime('2021-08-01'),50,
 	'東京オリンピック',color='k',ha='center', 
 	va='center',rotation='vertical',zorder=zorder);zorder+=1
+# ====
+plt.axvline(pd.to_datetime('2021-10-21'), 
+	color='k', linestyle='--',zorder=zorder);zorder+=1
+plt.text(pd.to_datetime('2021-10-21')-pd.DateOffset(4),50,
+	'衆議院選挙 (任期満了)', backgroundcolor='white',fontsize=15,
+	ha='center', va='center',
+	rotation='vertical',zorder=zorder);zorder+=1
+# ====
+plt.axvline(pd.to_datetime('2021-07-04'), 
+	color='k', linestyle='--',zorder=zorder);zorder+=1
+plt.text(pd.to_datetime('2021-07-04')+pd.DateOffset(4),50,
+	'都議会選挙 (選挙期日)', backgroundcolor='white',
+	ha='center', va='center',rotation='vertical',fontsize=15,
+	zorder=zorder);zorder+=1
+# ====
+plt.axvline(pd.to_datetime('2021-06-25'), 
+	color='k', linestyle='--',zorder=zorder);zorder+=1
+plt.text(pd.to_datetime('2021-06-25')-pd.DateOffset(4),50,
+	'都議会選挙 (告示日)', backgroundcolor='white',
+	ha='center', va='center',rotation='vertical',fontsize=15,
+	zorder=zorder);zorder+=1
+# ====
+
 ax1.fill_between(pd.to_datetime(date_array),np.add.accumulate(num1st_array)/total_population*100,
-	step="mid",color='#F08080',alpha=0.5,zorder=zorder);zorder+=1
+	step="mid",color='#F08080',alpha=0.5,label='１回目の接種人数',
+	zorder=zorder);zorder+=1
 ax1.step(pd.to_datetime(date_array),np.add.accumulate(num1st_array)/total_population*100,
-	'-',where='mid',markersize=0,label='総人口に対するワクチン接種率',
+	'-',where='mid',markersize=0,
 	color='r',linewidth=2,zorder=zorder);zorder+=1
+ax1.fill_between(pd.to_datetime(date_array),np.add.accumulate(num2nd_array)/total_population*100,
+	step="mid",color='#85C1E9',alpha=0.5,label='２回目の接種人数',
+	zorder=zorder);zorder+=1
+ax1.step(pd.to_datetime(date_array),np.add.accumulate(num2nd_array)/total_population*100,
+	'-',where='mid',markersize=0,
+	color='b',linewidth=2,zorder=zorder);zorder+=1
+
+expected_days = (total_population-np.add.accumulate(num1st_array)[-1])/(pol1_fit_param[0]*normalization)
+
 ax1.plot(prediction_date,
 	prediction_shots*normalization/total_population*100,
 	linestyle="--",color='r',zorder=zorder,
-	label='直近 %d 日間からの予測 (+%.1f 万人/日)' % (fit_width_days,pol1_fit_param[0]))
+	label='直近 %d 日間からの予測 (+%.1f 万人/日)\nこのペースでは全国民接種まで %d 日必要' % (fit_width_days,pol1_fit_param[0],expected_days))
 zorder+=1
 ax1.yaxis.label.set_color('red')
 ax1.set_xlim(date_start,'2021-10-31')
 ax1.set_ylim(0,100)
 ax1.set_xlabel('日付 (2021年)')
-ax1.set_ylabel('１回目のワクチン接種をした人の総人口に対する割合 (%)',
-	color='r',labelpad=20)
+ax1.set_ylabel('日本の総人口に対する積算のワクチン接種人数の割合 (%)',color='k',labelpad=20)
 myFmt = mdates.DateFormatter('%m/%d')
 ax1.xaxis.set_major_formatter(myFmt)
 fig.patch.set_alpha(0.0)
 ax1.patch.set_alpha(0.0) 
-plt.axvline(pd.to_datetime('2021-10-21'), 
-	color='k', linestyle='--',zorder=zorder);zorder+=1
-plt.text(pd.to_datetime('2021-10-21'),50,
-	'衆議院議員選挙 (任期満了)', backgroundcolor='white',
-	ha='center', va='center',
-	rotation='vertical',zorder=zorder);zorder+=1
-plt.axvline(pd.to_datetime('2021-07-22'), 
-	color='k', linestyle='--',zorder=zorder);zorder+=1
-plt.text(pd.to_datetime('2021-07-22'),50,
-	'東京都議会議員選挙 (任期満了)', backgroundcolor='white',
-	ha='center', va='center',rotation='vertical',
-	zorder=zorder);zorder+=1
-#second_ay = ax1.secondary_yaxis('right',functions=(number2ratio_oku, ratio2number_oku))
-#second_ay.set_ylabel('人口に占める割合 (%)')
 
 ax2 = ax1.twinx()  
 ax2.set_ylabel('全国の新型コロナウイルス陽性者数 (人)',color='#7D3C98',labelpad=20)  # we already handled the x-label with ax1
@@ -337,11 +364,13 @@ ax2.tick_params(axis='y',color='#7D3C98')
 ax2.step(df_positive['date'], df_positive['positive'],'o-',where='mid',
 	markersize=0,label='新型コロナウイルス陽性者(全国)',color='#7D3C98',
 	linewidth=2)
-#ax2.set_ylim(0,100.0)
+ax2.set_ylim(0,1.3*max(df_positive['positive']))
+#ax2.set_zorder(-1)
 
-ax1.legend(loc='upper left',borderaxespad=1,fontsize=20,ncol=1,
-	title='%s時点,首相官邸サイトに基づく' % date.today())
 
-fig.savefig("fig/covid19_positive_vaccine_accum_1st_prediction.pdf")
-fig.savefig("fig/covid19_positive_vaccine_accum_1st_prediction.jpg",dpi=300)
+ax1.legend(loc='upper left',borderaxespad=1,fontsize=18,ncol=1,
+	title='首相官邸サイトから作成 (%s時点)' % date.today()).set_zorder(zorder)
+
+fig.savefig("fig/covid19_positive_vaccine_accum_long.pdf")
+fig.savefig("fig/covid19_positive_vaccine_accum_long.jpg",dpi=300)
 
